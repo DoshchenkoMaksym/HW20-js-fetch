4ddd79622fd60d7b41a1ea1730d09180d2eb552b
Maksym Doshchenko <doschenko54@gmail.com>
2021-09-11 18:47:27 +0300

    fix2

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 let selectList = document.querySelector('.albums__list');
 let photosGallery = document.querySelector('.photos__gallery');
-getAlbums().then(() => getPhotos(selectList.value));
-
+getAlbums().then(() => getPhotos(1));
 
 
 async function getAlbums() {
     let albums = await fetch('https://jsonplaceholder.typicode.com/albums');
     let albumsArr = await albums.json();
     selectList.innerHTML = albumsArr.map(data => {
-        return `<li  class="albums__list-item" id="${data.id}">${data.id}. Album title: "${data.title}"</li>`;
+        return `<li  class="albums__list-item" data-id="${data.id}">${data.id}. Album title: "${data.title}"</li>`;
     }).join('');
 };
 
@@ -16,10 +15,9 @@ async function getPhotos(id) {
     let photos = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`);
     let photosArr = await photos.json();
     photosGallery.innerHTML = photosArr.map(data => {
-        let img = `<img class="photos__img" src="${data.thumbnailUrl}"></img>`;
-        return img;
+        return `<img class="photos__img" src="${data.thumbnailUrl}"></img>`;
     }).join('');
 };
 
-selectList.addEventListener('click', (event) => getPhotos((event.target.id)));
+selectList.addEventListener('click', (event) => getPhotos((event.target.getAttribute('data-id'))));
 
