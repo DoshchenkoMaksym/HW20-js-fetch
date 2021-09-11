8c3d27612f566f1954032775fdd09af8424518fd
Maksym Doshchenko <doschenko54@gmail.com>
2021-09-11 18:32:01 +0300

    fix1

--- main.js
+++ main.js
@@ -8,8 +8,7 @@ async function getAlbums() {
     let albums = await fetch('https://jsonplaceholder.typicode.com/albums');
     let albumsArr = await albums.json();
     selectList.innerHTML = albumsArr.map(data => {
-        let li = `<option  class="albums__list-item" value="${data.id}">${data.id}. Album title: "${data.title}"</option>`;
-        return li;
+        return `<li  class="albums__list-item" id="${data.id}">${data.id}. Album title: "${data.title}"</li>`;
     }).join('');
 };
 
@@ -22,5 +21,5 @@ async function getPhotos(id) {
     }).join('');
 };
 
-selectList.addEventListener('change', () => getPhotos(selectList.value));
+selectList.addEventListener('click', (event) => getPhotos((event.target.id)));
 
